62c0fec293cbcb3760744271b19901059001cbba
tina-le7617 <tina.le7617@gmail.com>
2022-06-01 12:17:55 -0400

    Finished My Store

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -8,10 +8,22 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
-
   items = this.cartService.getItems();
 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
   constructor(
-    private cartService: CartService
-    ) {}
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
